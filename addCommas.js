5b1e11059d0607ee6cd2a4a04e749ff119c886a1
Sean Bailey <seanbailey15@gmail.com>
2024-04-18 18:21:52 -0400

    completed part 2 of the assessment

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,50 @@
-function addCommas() {}
+/**
+ * This function takes a number as an argument.
+ * Converts the given number into a string formatted with commas
+ *
+ * @param {number} num
+ * @returns {string} The number, formatted with commas
+ */
+
+function addCommas(num) {
+  try {
+    if (typeof num !== "number") {
+      throw new Error("Argument must be a number");
+    }
+
+    let string = num.toString().split(".");
+
+    let digitPlace = 0;
+
+    let converted = "";
+
+    for (let i = string[0].length - 1; i >= 0; i--) {
+      digitPlace += 1;
+
+      if (string[0][i] === "-") {
+        converted = string[0][i] + converted;
+      } else if (
+        (digitPlace % 3 === 0 && i === 0) ||
+        (string[0][0] === "-" && string[0].length === 4)
+      ) {
+        converted = string[0][i] + converted;
+      } else if (digitPlace % 3 === 0) {
+        let commaPlusDigit = "," + string[0][i];
+        converted = commaPlusDigit + converted;
+      } else {
+        converted = string[0][i] + converted;
+      }
+    }
+
+    if (string.length === 2) {
+      console.log(converted + string[1]);
+      return converted + "." + string[1];
+    }
+    console.log(converted);
+    return converted;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,71 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("it converts positive number, three digits or less, without a comma", () => {
+    let result = addCommas(123);
+    expect(result).toEqual("123");
+
+    result = addCommas(12);
+    expect(result).toEqual("12");
+
+    result = addCommas(1);
+    expect(result).toEqual("1");
+  });
+
+  test("it converts positive number of four digits with a comma", () => {
+    let result = addCommas(1234);
+    expect(result).toEqual("1,234");
+  });
+
+  test("it converts positive number of seven digits with two commas", () => {
+    let result = addCommas(1234567);
+    expect(result).toEqual("1,234,567");
+  });
+
+  test("it converts positive number of ten digits with three commas", () => {
+    let result = addCommas(1234567890);
+    expect(result).toEqual("1,234,567,890");
+  });
+
+  test("it converts negative number, three digits or less, without a comma", () => {
+    let result = addCommas(-123);
+    expect(result).toEqual("-123");
+
+    result = addCommas(-12);
+    expect(result).toEqual("-12");
+
+    result = addCommas(-1);
+    expect(result).toEqual("-1");
+  });
+
+  test("it converts negative number of four digits with a comma", () => {
+    let result = addCommas(-1234);
+    expect(result).toEqual("-1,234");
+  });
+
+  test("it converts negative number of seven digits with two commas", () => {
+    let result = addCommas(-1234567);
+    expect(result).toEqual("-1,234,567");
+  });
+
+  test("it converts negative number of ten digits with three commas", () => {
+    let result = addCommas(-1234567890);
+    expect(result).toEqual("-1,234,567,890");
+  });
+
+  test("it converts positive number with decimal properly", () => {
+    let result = addCommas(12345.678);
+    expect(result).toEqual("12,345.678");
+  });
+
+  test("it converts negative number with decimal properly", () => {
+    let result = addCommas(-12345.678);
+    expect(result).toEqual("-12,345.678");
+  });
+
+  test("it converts larger negative number with decimal properly", () => {
+    let result = addCommas(-3141592.64);
+    expect(result).toEqual("-3,141,592.64");
+  });
 });
